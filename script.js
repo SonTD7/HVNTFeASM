2b1d97ad9e3df4ddf2c80aa9d7333008c4ae8aa4
SonTD7 <sontddeveloper@gmail.com>
2022-05-05 18:15:32 +0700

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function myFunction() {
+  let name = prompt("Please enter your name", "Harry Potter");
+  if (name != null) {
+    document.getElementById("ten").innerHTML =
+    "Hello " + person + "! How are you today?";
+  }
+}
+
+document.getElementById('hien').onclick = hienLen;
+function hienLen() {
+
+}
