7be746c74930e023afb8ce5d6a246af033f0b561
FER201m-SP22 <sontddeveloper@gmail.com>
2022-05-13 22:27:31 +0700

    commit

--- ASMJS-1/script.js
+++ ASMJS-1/script.js
@@ -1,49 +1,117 @@
+// document.getElementById("hien").onclick = hienLen;
+// function hienLen() {
+//   alert("Chào mừng đến với ứng dụng JavaScript đầu tiên.");
+//   hienLenx("Yêu cầu đồng chí nhập họ và tên", "Tran Duy Son", "yeu cau nhap lai","ten",
+//   false);
+//   hienLenx("Thành phố", "Quảng Ngãi", "phai nhap ten thanh pho" ,"tp",true);
+//   hienLenx("Số điện thoại", "0972******", "phai nhap dung so dien thoai" ,"sdt",true);
+// }
+// // function hienLen2() {
+// //   let name = prompt("Yêu cầu đồng chí nhập họ và tên", "Trần Duy Sơn");
+// //   if (name == "" || !isNaN(name)) {
+// //     alert("Tên Việt Nam không có số má ưi. -.-");
+
+// //     hienLen2();
+// //   } else {
+// //     document.getElementById("ten").innerHTML = "Họ và tên: " + name;
+// //     hienLen3();
+// //   }
+// // }
+
+// function hienLenx(message,defaultValue,errorMessage,tagId) {
+//   let name = prompt(message,defaultValue);
+//   if (name == "" || !isNaN(name)) {
+//     alert(errorMessage);
+
+//     hienLenx("Yêu cầu đồng chí nhập họ và tên", "Tran Duy Son","Tên Việt Nam không có số má ưi. -.-", "ten",false);
+//   } else {
+//     document.getElementById(tagId).innerHTML = name;
+
+//   }
+// }
+
+// function hienLen3() {
+//   let nameTp = prompt("Thành phố", "Quảng Ngãi");
+//   if (nameTp == null) {
+//     alert("phai dien vao")
+//     hienLen3();
+//   } else {document.getElementById("tp").innerHTML = "Thuộc thành phố: " + nameTp;
+//   hienLen4();}
+
+// }
+// function hienLen4() {
+//   let nameSo = prompt("Số điện thoại", "0972******");
+//   if (nameSo == "" || isNaN(nameSo)) {
+//     alert(
+//       "Yêu cầu đồng chí nhập số điện thoại chứ không phải nhập chữ điện thoại :))"
+//     );
+
+//     hienLen4();
+//   } else {
+//     document.getElementById("sdt").innerHTML = "Liên hệ: " + nameSo;
+//     hienLen5();
+//   }
+
+// }
+// function hienLen5() {
+//   var now = new Date().getFullYear();
+//   let nameNs = prompt("Năm sinh", "2001");
+//   if (nameNs > now || nameNs == "" || isNaN(nameNs) || nameNs < 1990) {
+//     alert("Phải nhập số nhỏ hơn năm hiện tại và lớn hơn 1990 cơ. -.-");
+
+//     hienLen5();
+//   } else {
+//     document.getElementById("nsinh").innerHTML = "Năm sinh: " + nameNs;
+//   }
+// }
+
 document.getElementById("hien").onclick = hienLen;
 function hienLen() {
   alert("Chào mừng đến với ứng dụng JavaScript đầu tiên.");
-  hienLen2();
-}
-function hienLen2() {
-  let name = prompt("Yêu cầu đồng chí nhập họ và tên", "Trần Duy Sơn");
-  if (name == "" || !isNaN(name)) {
-    {
-    alert("Tên Việt Nam không có số má ưi. -.-");
-    hienLen2();
-  }
-    
-  }  else {
-    document.getElementById("ten").innerHTML = "Họ và tên: " + name;
-    
-  }
-  hienLen3();
-}
-function hienLen3() {
-  let name = prompt("Thành phố", "Quảng Ngãi");
-  if (name != null) {
-    document.getElementById("tp").innerHTML = "Thuộc thành phố: " + name;
-  }
-  hienLen4();
-}
-function hienLen4() {
-  let nameSo = prompt("Số điện thoại", "0972******");
-  if (nameSo == "" || isNaN(nameSo)) {
-    {
-      alert("Yêu cầu đồng chí nhập số điện thoại chứ không phải nhập chữ điện thoại :))")
+  let name;
+  do {
+    name = prompt("Yêu cầu đồng chí nhập họ và tên", "Trần Duy Sơn");
+    if (name == "" || !isNaN(name)) {
+      alert("Tên Việt Nam không có số má ưi. -.-");
+    } else {
+      document.getElementById("ten").innerHTML = name;
     }
-  } else{
-    document.getElementById("sdt").innerHTML = "Liên hệ: " + nameSo;
-  }
-  hienLen5();
-}
-function hienLen5() {
-  var now = new Date().getFullYear();
-  let nameNs = prompt("Năm sinh", "2001");
-  if (nameNs > now || nameNs == "" || isNaN(nameNs) || nameNs < 1990) {
-    {
-      alert("Phải nhập số nhỏ hơn năm hiện tại và lớn hơn 1990 cơ. -.-");
-      hienLen5();
+  } while (name == "" || !isNaN(name));
+  let nameTp;
+  do {
+    nameTp = prompt("Thành phố", "Quảng Ngãi");
+    if (nameTp == "") {
+      alert("Không được để trống.");
+    } else {
+      document.getElementById("tp").innerHTML = nameTp;
     }
-  } else {
-    document.getElementById("nsinh").innerHTML = "Năm sinh: " + nameNs;
-  }
+  } while (nameTp == "");
+  let nameSo;
+  do {
+    nameSo = prompt("Số điện thoại", "0972******");
+    if (nameSo == "" || isNaN(nameSo)) {
+      alert(
+        "Yêu cầu đồng chí nhập số điện thoại chứ không phải nhập chữ điện thoại :))"
+      );
+    } else {
+      document.getElementById("sdt").innerHTML = nameSo;
+    }
+  } while (nameSo == "" || isNaN(nameSo));
+  let nameNs;
+  do{
+    nameNs = prompt("Năm sinh", "2001");
+    if(nameNs == "" || isNaN(nameNs)){
+alert("Phải nhập số nhỏ hơn năm hiện tại và lớn hơn 1990 cơ. -.-");
+    } else{
+      document.getElementById("nsinh").innerHTML = nameNs;
+    }
+  
+  } while(nameNs == "" || isNaN(nameNs));
+  
 }
+document.getElementById("len").style.display = 'none';
+document.getElementById("showMessage").style.borderBlockColor = 'pink';
+document.getElementById("showMessage").style.backgroundColor = 'green';
+document.getElementById("showMessage").onclick = function () {  
+  document.getElementById("len").style.display = 'block';
+};
